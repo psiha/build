4862d81f848999ed9ada9ef6cecb6a402005b67c
Domagoj Šarić <dsaritz@gmail.com>
2016-05-10 23:06:22 +0200

    Added the utilities module (for now just with the TNUN_make_temp_path() function).

---
+++ utilities.cmake
@@ -0,0 +1,28 @@
+################################################################################
+#
+# T|N.U.N. CMake utilities
+#
+# Copyright (c) 2016. Domagoj saric. All rights reserved.
+#
+################################################################################
+
+cmake_minimum_required( VERSION 3.0 )
+
+
+################################################################################
+# TNUN_make_temp_path()
+################################################################################
+
+function( TNUN_make_temp_path file_name_variable )
+    if    ( EXISTS $ENV{TEMP} )
+        set( temp_dir "$ENV{TEMP}" )
+    elseif( EXISTS $ENV{TMP} )
+        set( temp_dir "$ENV{TMP}" )
+    elseif( EXISTS $ENV{TMPDIR} )
+        set( temp_dir "$ENV{TMPDIR}" )
+    else()
+        set( temp_dir "${PROJECT_BINARY_DIR}" )
+    endif()
+    set( file_name ${${file_name_variable}} )
+    set( ${file_name_variable} "${temp_dir}/${file_name}" PARENT_SCOPE )
+endfunction()
