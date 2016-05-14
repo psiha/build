424b04218e6b08f53138bf0554409070dd4eec55
Domagoj Šarić <dsaritz@gmail.com>
2016-05-14 20:13:31 +0200

    Windows: silenced the 'structure was padded due to alignment specifier' warning.

--- toolchains/msvc.toolchain.cmake
+++ toolchains/msvc.toolchain.cmake
@@ -19,7 +19,7 @@ set( TNUN_compiler_optimize_for_speed /Ox /Ot /Qvec-report:2                    
 
 set( TNUN_compiler_release_flags /DNDEBUG /Bt /Ox /Ob2 /Oy /GF /Gw /Gm- /GS- /Gy /Qpar )
 
-add_compile_options( /MP /Oi /W4 /Zc:threadSafeInit- )
+add_compile_options( /MP /Oi /W4 /Zc:threadSafeInit- /wd4324 ) # w4324 = 'structure was padded due to alignment specifier'
 add_definitions(
     -D_CRT_SECURE_NO_WARNINGS
     -D_SBCS
