e1b9a4c7cbfaeaa2f76c2a74c240dba914f2c925
Domagoj Šarić <dsaritz@gmail.com>
2016-05-24 12:08:11 +0200

    Added a placeholder for future PCH support.

---
+++ pch.cmake
@@ -0,0 +1,10 @@
+################################################################################
+#
+# T|N.U.N. Build PCH support
+#
+# Copyright (c) 2016. Domagoj saric. All rights reserved.
+#
+################################################################################
+
+#...todo...
+# http://clang.llvm.org/docs/PCHInternals.html
