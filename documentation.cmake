58fefbb1250e3b8ec62a76d50360144f8e96feaf
Domagoj Šarić <dsaritz@gmail.com>
2016-05-31 14:20:41 +0200

    Fixed/added conversion between CMake 'options' and Doxygen 'boolean' configuration options.

--- documentation.cmake
+++ documentation.cmake
@@ -29,30 +29,37 @@
 
 set( TNUN_dir "${CMAKE_CURRENT_LIST_DIR}" )
 
+macro( TNUN_internal_cmake2doxy_boolean boolean )
+  string( REPLACE "OFF" "NO"  ${boolean} ${${boolean}} )
+  string( REPLACE "ON"  "YES" ${boolean} ${${boolean}} )
+endmacro()
+
 function( TNUN_add_documentation )
 
+    # https://cmake.org/pipermail/cmake/2011-January/042313.html
+    find_package( Doxygen )
     if ( NOT DOXYGEN_FOUND )
-        find_package( Doxygen )
-        if ( NOT DOXYGEN_FOUND )
-            message( WARNING "Unable to find Doxygen" )
-            return()
-        endif()
+        message( WARNING "Unable to find Doxygen" )
+        return()
     endif()
     
     if ( NOT DEFINED TNUN_DOC_STAGE_PATH )
         set( TNUN_DOC_STAGE_PATH "${PROJECT_BINARY_DIR}/doc" )
     endif()
 
-    option( TNUN_DOC_CLANG_ASSISTED_PARSING "Use Clang for parsing sources when generating the documentation (more accurate but slower)" )
+    option( TNUN_DOC_CLANG_ASSISTED_PARSING "Use Clang for parsing sources when generating the documentation (more accurate but slower)" NO )
     if ( TNUN_DOC_CLANG_ASSISTED_PARSING )
         set( TNUN_DOXYGEN_CLANG_OPTIONS "-fms-extensions -fdelayed-template-parsing -fmsc-version=1900 -Wno-ignored-attributes -Wno-multichar -D\"BOOST_PP_CONFIG_FLAGS()\"=1 -DDOXYGEN_ONLY" )
     endif()
 
     option( TNUN_DOC_SHOW_NAMESPACES  YES )
-    option( TNUN_DOC_HIDE_SCOPE_NAMES YES )
+    option( TNUN_DOC_HIDE_SCOPE_NAMES NO  )
 
     # Convert CMake list to a Doxygen-friendly "\ + newline" separated list.
     string( REPLACE ";" " \\\n\t" TNUN_DOC_SOURCES "${TNUN_DOC_SOURCES}" )
+    TNUN_internal_cmake2doxy_boolean( TNUN_DOC_SHOW_NAMESPACES  )
+    TNUN_internal_cmake2doxy_boolean( TNUN_DOC_HIDE_SCOPE_NAMES )
+    TNUN_internal_cmake2doxy_boolean( TNUN_DOC_CLANG_ASSISTED_PARSING )
     configure_file(
         "${TNUN_dir}/doxyfile.in"
         "${PROJECT_BINARY_DIR}/doxyfile"
