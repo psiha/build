add9304d514ac24fc509054887a82d761053d379
Nenad Mikša <nenad.miksa@microblink.com>
2016-06-30 10:50:39 +0200

    disabled undefined sanitization on Mac OS X, as it is not supported there

--- toolchains/clang.cmake
+++ toolchains/clang.cmake
@@ -13,7 +13,10 @@ include( "${CMAKE_CURRENT_LIST_DIR}/gcc_compatibles.cmake" )
 list( APPEND TNUN_compiler_optimize_for_speed -fvectorize -fslp-vectorize -fslp-vectorize-aggressive )
 list( APPEND TNUN_compiler_optimize_for_speed -Rpass=loop-.* ) #-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -mllvm -bb-vectorize-aligned-only
 
-# http://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
-set( TNUN_compiler_runtime_sanity_checks -fsanitize=undefined -fsanitize=integer )
+# https://github.com/P-H-C/phc-winner-argon2/issues/103
+if(NOT APPLE)
+    # http://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
+    set( TNUN_compiler_runtime_sanity_checks -fsanitize=undefined -fsanitize=integer )
+endif()
 
 add_compile_options( -Wheader-guard )
