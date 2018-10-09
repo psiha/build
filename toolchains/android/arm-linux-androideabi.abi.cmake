7956c3d85066bcc722c810d82548e28ba46639f6
Domagoj Šarić <dsaritz@gmail.com>
2018-10-09 10:47:13 +0200

    Fixed Android 32bit ARM ABI file to match settings as set by the current NDK toolchain file.

--- toolchains/android/arm-linux-androideabi.abi.cmake
+++ toolchains/android/arm-linux-androideabi.abi.cmake
@@ -2,7 +2,7 @@
 #
 # T:N.U.N. Android ABI config file.
 #
-# Copyright (c) 2016. Domagoj Saric. All rights reserved.
+# Copyright (c) 2016. - 2018. Domagoj Saric. All rights reserved.
 #
 ################################################################################
 
@@ -10,19 +10,19 @@
 set( TNUN_cpu_archs
   # Implementation note: Dropping support for the old armeabi archs greatly
   # simplifies things: CMAKE_SYSTEM_PROCESSOR and ANDROID_NDK_ABI_NAME become
-  # fixed (armv7a and armeabi-v7a, respectively) just like in other ABIs.
+  # fixed (armv7-a and armeabi-v7a, respectively) just like in other ABIs.
   #                                           (03.06.2016.) (Domagoj Saric)
   #armv5te_armeabi ... obsolete
   #armv6m-vfp2_armeabi ... obsolete ... -marm -march=armv6k -mtune=arm1136j-s
   armv7-vfp3d16
- #armv7-vfp3d32 ... rare
+  armv7-vfp3d32
   armv7-neon
 )
 
 set( abi_sufix                "eabi"                          )
 set( ANDROID_ARCH_NAME        "arm"                           )
 set( ANDROID_GCC_MACHINE_NAME "arm-linux-android${abi_sufix}" )
-set( CMAKE_SYSTEM_PROCESSOR   "armv7"                         )
+set( CMAKE_SYSTEM_PROCESSOR   "armv7-a"                       )
 
 list( APPEND TNUN_compiler_optimize_for_size  -mthumb )
 list( APPEND TNUN_compiler_optimize_for_speed -marm   )
