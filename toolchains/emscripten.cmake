b602e89434c4de126d8b588c25a00954379f25ce
Nenad Mikša <nenad.miksa@microblink.com>
2019-09-03 20:22:49 +0200

    cyberdwarf is available only with fastcomp backend

--- toolchains/emscripten.cmake
+++ toolchains/emscripten.cmake
@@ -8,8 +8,18 @@
 
 include( "${CMAKE_CURRENT_LIST_DIR}/clang.cmake" )
 
-set( TNUN_compiler_debug_symbols -g4 "SHELL:-s DEMANGLE_SUPPORT=1" "SHELL:-s CYBERDWARF=1" )
-set( TNUN_linker_debug_symbols   -g4 "SHELL:-s DEMANGLE_SUPPORT=1" "SHELL:-s CYBERDWARF=1" )
+set( TNUN_compiler_debug_symbols -g4 "SHELL:-s DEMANGLE_SUPPORT=1" )
+set( TNUN_linker_debug_symbols   -g4 "SHELL:-s DEMANGLE_SUPPORT=1" )
+
+string( REPLACE "." ";" VERSION_LIST ${CMAKE_CXX_COMPILER_VERSION} )
+list( GET VERSION_LIST 0 clang_major_version  )
+
+# if fastcomp backend, enable cyberdwarf debugger
+if ( ${clang_major_version} EQUAL 6 )
+    list( APPEND TNUN_compiler_debug_symbols "SHELL:-s CYBERDWARF=1" )
+    list( APPEND TNUN_linker_debug_symbols   "SHELL:-s CYBERDWARF=1" )
+endif()
+
 
 set( TNUN_linker_exceptions_on  "SHELL:-s DISABLE_EXCEPTION_CATCHING=0" )
 set( TNUN_linker_exceptions_off "SHELL:-s DISABLE_EXCEPTION_CATCHING=1" )
